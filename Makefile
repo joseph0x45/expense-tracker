de3101c6ede07af0aa2be6ab8253dda2c479cf40
joseph0x45 <zozozozeph@gmail.com>
2025-01-13 16:24:49 +0000

    moving over to vuejs

--- Makefile
+++ Makefile
@@ -7,4 +7,3 @@ build:
 
 start-tailwind:
 	npx tailwindcss -i ./public/input.css -o ./public/output.css --watch
->>>>>>> parent of d79ef98 (little bit of refactoring)
--- main.go
+++ main.go
@@ -6,15 +6,7 @@ import (
 	"log"
 	"net/http"
 	"text/template"
-	"embed"
-	"flag"
-	"fmt"
-	"io/fs"
-	"log"
-	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"os"
+	"time"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
@@ -22,35 +14,9 @@ import (
 
 var (
 	DB *sqlx.DB
-
-	//go:embed frontend/dist
-	frontend embed.FS
 )
 
 var buildMode = "prod"
-
-func init() {
-	userHomeDir, err := os.UserHomeDir()
-	if err != nil {
-		panic(err)
-	}
-	dbFilePath := fmt.Sprintf("%s/.cashflow.db", userHomeDir)
-	_, err = os.Stat(dbFilePath)
-	if os.IsNotExist(err) {
-		initDB(dbFilePath)
-	} else {
-		DB, err = sqlx.Connect("sqlite3", dbFilePath)
-		if err != nil {
-			panic(err)
-		}
-	}
-	log.Println("Connected to database")
-)
-
-type film struct {
-	Title    string
-	Director string
-}
 
 func renderHomePage(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("./home.html"))
@@ -61,22 +27,6 @@ func main() {
 	port := flag.String("port", "8080", "Port on which to launch the app")
 	flag.Parse()
 	mux := http.NewServeMux()
-	if buildMode == "dev" {
-    log.Println("Running in dev mode")
-		frontendURL, err := url.Parse("http://localhost:5173")
-		if err != nil {
-			panic(err)
-		}
-		proxy := httputil.NewSingleHostReverseProxy(frontendURL)
-		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			proxy.ServeHTTP(w, r)
-		})
-	} else {
-		dist, _ := fs.Sub(frontend, "frontend/dist")
-		mux.Handle("/", http.FileServer(http.FS(dist)))
-	}
-
-	mux.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
 
 	mux.HandleFunc("/", renderHomePage)
 
