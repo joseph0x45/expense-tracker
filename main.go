5572da3512b9bba5e5ee8f020c0065e6672fc1ab
joseph0x45 <zozozozeph@gmail.com>
2025-01-19 08:12:07 +0000

    remove moree stuff

--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"embed"
 	"flag"
 	"fmt"
-	"io/fs"
 	"log"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
 	"time"
 
 	"github.com/jmoiron/sqlx"
@@ -16,31 +12,13 @@ import (
 )
 
 var (
-	DB      *sqlx.DB
-	//go:embed frontend/dist
-	frontend embed.FS
+	DB *sqlx.DB
 )
-
-var buildMode = "prod"
 
 func main() {
 	port := flag.String("port", "8080", "Port on which to launch the app")
 	flag.Parse()
 	mux := http.NewServeMux()
-
-	if buildMode == "dev" {
-		frontendURL, err := url.Parse("http://localhost:5173")
-		if err != nil {
-			panic(err)
-		}
-		proxy := httputil.NewSingleHostReverseProxy(frontendURL)
-		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			proxy.ServeHTTP(w, r)
-		})
-	} else {
-		dist, _ := fs.Sub(frontend, "frontend/dist")
-		mux.Handle("/", http.FileServer(http.FS(dist)))
-	}
 
 	server := http.Server{
 		Addr:         fmt.Sprintf(":%s", *port),
